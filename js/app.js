3ed508cf9ed6c8d45feee30df7c9734b5d66f9c8
ibrahimsifat <ibsifat900@gmail.com>
2021-09-13 19:37:16 +0300

    html updated

--- js/app.js
+++ js/app.js
@@ -15,15 +15,17 @@ const showProducts = (products) => {
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${product.image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" class="btn btn-danger" onclick="productDetails('${product.id}')">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
+     // console.log(product.price)
+
   }
 };
 let count = 0;
@@ -33,11 +35,12 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal()
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,26 +49,26 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
-  if (priceConverted > 200) {
+  if (priceConverted > 200 &&priceConverted > 2) {
     setInnerText("delivery-charge", 30);
     setInnerText("total-tax", priceConverted * 0.2);
   }
-  if (priceConverted > 400) {
+  if (priceConverted > 400 &&priceConverted > 2) {
     setInnerText("delivery-charge", 50);
     setInnerText("total-tax", priceConverted * 0.3);
   }
-  if (priceConverted > 500) {
+  if (priceConverted > 500 &&priceConverted > 2) {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
@@ -76,6 +79,34 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+    
+  document.getElementById("total").innerText = grandTotal.toFixed();
 };
-//form phone 
+
+//detais section 
+const productDetails=id=>{
+  const URL=`https://fakestoreapi.com/products/${id}`
+  fetch(URL)
+ .then(res=>res.json())
+.then(data=>displayDetails(data))
+  //console.log(id)
+}
+productDetails()
+const displayDetails=details=>{
+  const productDetailsContainer=document.getElementById('product-details')
+const card=document.getElementById('card')
+ card.innerHTML=`
+  
+  <img src="${details.image}" class="card-img-top img-fluid" alt="Product Image">
+        <div class="card-body">
+        <h3>${details.title}</h3>
+      <p>Category: ${details.category}</p>
+      <h2>Price: $ ${details.price}</h2>
+      <button onclick="addToCart(${details.id},${details.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+          <p class="card-text">${details.description}.</p>
+        </div>
+  
+  `
+  productDetailsContainer.appendChild(card)
+  console.log(details)
+}
